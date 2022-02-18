122a8652929ea53e3023208f54a3966b36f2b472
Marlena Bannick <mnorwood@uw.edu>
2022-02-18 07:37:24 -0800

    added a summary simulations file

--- simulations/sim-summary-2.R
+++ simulations/sim-summary-2.R
@@ -2,7 +2,8 @@ rm(list=ls())
 library(data.table)
 library(ggplot2)
 
-in_dir <- "~/Documents/FileZilla/rct/run-17-02-22-5/"
+args <- commandArgs(TRUE)
+in_dir <- args[1]
 setwd(in_dir)
 
 files <- list.files(in_dir, ".RData")
@@ -38,30 +39,5 @@ df[, type := factor(type, levels=c("un-adjusted", "adjusted",
                                    "inconsistent, naive", "inconsistent, corrected"))]
 
 df[, n := as.factor(n)]
+write.csv(df, file=paste0(in_dir, "/summary.csv"), row.names=F)
 
-# Type I Error Plot
-
-pdf("~/OneDrive/Documents/2021-2022/BIOST 600-noah/type1error.pdf", height=8, width=14)
-ggplot(data=df[delta == 0.0 & stages == 3], aes(x=rho, y=power, color=type, linetype=afunc)) +
-  facet_grid(~ n) +
-  geom_point() +
-  geom_hline(yintercept=0.05, linetype="dashed", color="black") +
-  geom_line() +
-  theme_minimal() +
-  scale_color_brewer(palette="Set1") +
-  theme(legend.position="top")
-  # geom_errorbar(aes(ymin=lower, ymax=upper))
-dev.off()
-
-ggplot(data=df[delta > 0 & stages == 3], aes(x=rho, y=power, color=n, linetype=afunc)) +
-  facet_grid(type ~ delta) +
-  geom_point() + geom_line()
-
-pdf("~/OneDrive/Documents/2021-2022/BIOST 600-noah/power.pdf", height=8, width=14)
-ggplot(data=df[delta == 0.1 & stages == 3], aes(x=rho, y=power, color=type, linetype=afunc)) +
-  facet_grid( ~ n) +
-  geom_point() + geom_line() +
-  scale_color_brewer(palette="Set1") +
-  theme_minimal() +
-  theme(legend.position="top")
-dev.off()

d6a179f926921eb4c70e05adcaa3c0f632cba7cf
Marlena Bannick <mnorwood@uw.edu>
2022-05-24 09:24:13 -0700

    updated simulation figures

--- simulations/sim-figures.R
+++ simulations/sim-figures.R
@@ -1,11 +1,9 @@
 library(data.table)
 library(ggplot2)
 
-df <- fread("/Users/marlena/Documents/FileZilla/rct/run-24-02-22-2/summary.csv")
-df0 <- fread("/Users/marlena/Documents/FileZilla/rct/run-10-04-22-2/summary.csv")
+# df <- fread("/Users/marlena/Documents/FileZilla/rct/run-24-02-22-2/summary.csv")
+# df0 <- fread("/Users/marlena/Documents/FileZilla/rct/run-10-04-22-2/summary.csv")
 df <- fread("/Users/marlena/Documents/FileZilla/rct/run-16-05-22-5/summary.csv")
-
-
 
 df <- df[((type %in% c("un-adjusted", "adjusted",
            "inconsistent, naive", "inconsistent, corrected")) & est_bounds == TRUE) |
@@ -14,8 +12,8 @@ df <- df[((type %in% c("un-adjusted", "adjusted",
 df[type == "un-adjusted", label := "(A)(i)"]
 df[type == "adjusted", label := "(A)(ii)"]
 df[type == "inconsistent, naive", label := "(B)(i)"]
-df[type == "inconsistent, corrected" & est_bounds == TRUE, label := "(B)(iii)"]
 df[type == "inconsistent, corrected" & est_bounds == FALSE, label := "(B)(ii)"]
+df[type == "inconsistent, corrected" & est_bounds == TRUE, label := "(B)(iii)"]
 
 df[, n := as.factor(n)]
 df[, std := sqrt(power * (1 - power)) / sqrt(10000)]
@@ -27,7 +25,7 @@ df[afunc == "pocock", bound_type := "Pocock"]
 
 # Type I Error Plot
 
-pdf("~/repos/Group-Sequential-Trials-Paper/figures/type1error.pdf", height=5, width=10)
+pdf("~/repos/Group-Sequential-Trials-Paper/figures/type1error-2.pdf", height=5, width=10)
 ggplot(data=df[delta == 0.0 & stages == 3], aes(x=1-rho, y=power, color=label)) +
   facet_grid(bound_type ~ n) +
   geom_point() +
@@ -47,7 +45,7 @@ dev.off()
 #   facet_grid(type ~ delta) +
 #   geom_point() + geom_line()
 
-pdf("~/repos/Group-Sequential-Trials-Paper/figures/power.pdf", height=5, width=10)
+pdf("~/repos/Group-Sequential-Trials-Paper/figures/power-2.pdf", height=5, width=10)
 ggplot(data=df[delta == 0.1 & stages == 3], aes(x=1-rho, y=power, color=label)) +
   facet_grid(bound_type ~ n) +
   geom_point() + geom_line() +

836d02fa6e531349453aae2626f52eabe721439e
Marlena Bannick <mnorwood@uw.edu>
2022-05-30 14:15:12 -0700

    reorganized simulation files.

--- simulations/sim-summary.R
+++ simulations/sim-summary.R
@@ -1,168 +1,79 @@
 rm(list=ls())
+library(data.table)
+library(ggplot2)
+library(magrittr)
 
-in_dir <- "~/Documents/FileZilla/rct/"
+args <- commandArgs(TRUE)
+in_dir <- args[1]
+# in_dir <- "~/Documents/FileZilla/rct/run-10-04-22-2/"
+# in_dir <- "~/Documents/FileZilla/rct/run-11-04-22-3/"
+# in_dir <- "~/Documents/FileZilla/rct/run-08-05-22-2/"
+# in_dir <- "~/Documents/FileZilla/rct/run-09-05-22-1/"
+# in_dir <- "~/Documents/FileZilla/rct/run-09-05-22-1/"
+# in_dir <- "~/Documents/FileZilla/rct/run-18-04-22-1"
+# in_dir <- "~/Documents/FileZilla/rct/run-16-05-22-2/"
 setwd(in_dir)
 
-stds <- c(0.001, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.08, 0.1, 0.115, 0.2, 0.3, 0.75)
-tasks <- 80
-N <- 800000
+files <- list.files(in_dir, ".RData")
 
-for(st in stds){
-  cat("\n", st)
-  var_stage <- sprintf("stage_%s", st)
-  var_tstat <- sprintf("tstat_%s", st)
-  var_smean <- sprintf("smean_%s", st)
+parameters <- fread("params.csv")
+setnames(parameters, "V1", "param")
 
-  assign(var_stage, c())
-  assign(var_tstat, c())
-  assign(var_smean, c())
+stage_num <- unique(parameters$stages)
+if(length(stage_num) > 1) stop()
+potential_stages <- paste0("stage_", 1:stage_num)
 
-  for(i in 1:80){
-    cat(".")
-    load(sprintf("task_%s_std_%s.RData", i, st))
-    early <- result2$l.bounds[, 2]
-    stage <- rep(1, length(early))
-    stage[early] <- 2
+get.results <- function(file){
+  param_id <- gsub("params_", "", file)
+  param_id <- gsub(".RData", "", param_id) %>% as.integer()
+  print(param_id)
 
-    tstat <- result2$tstat
-    smean <- result2$smean
+  truth <- c(parameters[param == param_id, delta])
 
-    assign(var_stage, c(get(var_stage), stage))
-    assign(var_tstat, c(get(var_tstat), tstat))
-    assign(var_smean, c(get(var_smean), smean))
-  }
+  load(file)
+  power <- mean(result$reject)
+  bias_naive <- mean(result$est - truth)
+  bias_corr <- mean(result$point - truth)
+  bias_naive_med <- median(result$est - truth)
+  bias_corr_med <- median(result$point - truth)
+
+  cover_naive <- mean((result$naive_ci[, 1] <= truth) & (result$naive_ci[, 2] >= truth))
+  cover_corr <- mean(result$ci[, 1] <= truth & (result$ci[, 2] >= truth))
+
+  bounds <- result$u.bounds
+  numbounds <- function(vec) sum(vec != 0)
+  stages <- apply(bounds, MARGIN=1, FUN=numbounds)
+
+  stageprop <- function(num) mean(stages == num)
+  stageprops <- sapply(1:stage_num, stageprop)
+
+  return(c(param_id, power,
+           bias_naive, bias_corr,
+           bias_naive_med, bias_corr_med,
+           cover_naive, cover_corr,
+           stageprops))
 }
 
-# This is how to get rho from the observation standard deviations
-# sim_stds <- c(0.001, 0.115)
-rho <- function(x) sqrt(x**2 / (0.1**2 + x**2))
-sim_rho <- rho(stds)
+results <- sapply(files, get.results)
+results <- t(results)
+results <- data.table(results)
+setnames(results, c("param", "power", "bias_naive", "bias_corr",
+                    "bias_naive_med", "bias_corr_med",
+                    "cover_naive", "cover_corr",
+                    potential_stages))
 
-# holders for p-value estimates
-sim_pvals_smean <- c()
-sim_pvals_tstat <- c()
+df <- merge(parameters, results, by="param")
+# df[, std := sqrt(power * (1 - power)) / sqrt(1000)]
+# df[, lower := power - qnorm(0.975) * std]
+# df[, upper := power + qnorm(0.975) * std]
 
-# standard errors of p-values
-sim_pvals_smean_se <- c()
-sim_pvals_tstat_se <- c()
+df[monitor == "anova" & final == "anova", type := "un-adjusted"]
+df[monitor == "ancova" & final == "ancova", type := "adjusted"]
+df[monitor == "anova" & final == "ancova" & correct == TRUE, type := "inconsistent, corrected"]
+df[monitor == "anova" & final == "ancova" & correct == FALSE, type := "inconsistent, naive"]
+df[, type := factor(type, levels=c("un-adjusted", "adjusted",
+                                   "inconsistent, naive", "inconsistent, corrected"))]
 
-# observed values on the boundaries
-obs_smean <- -1.969/sqrt(100)
-obs_tstat <- -1.969
-
-# loop through each of the observation standard deviations
-for(st in stds){
-
-  std_name <- sprintf("%03d", st*1000)
-
-  # CALCULATE SAMPLE MEAN P-VALUE EST AND P-VALUE SE
-  smean_sims <- get(sprintf("smean_%s", st))
-  # Take fraction of time that simulation is more extreme than observed (two-sided)
-  pval_smean <- mean((smean_sims <= -abs(obs_smean)) | (smean_sims >= abs(obs_smean)))
-  pval_smean_se <- sqrt(pval_smean * (1 - pval_smean) / N)
-
-  # CALCULATE STAGE-WISE MEAN P-VALUE EST AND P-VALUE SE
-  tstat_sims <- get(sprintf("tstat_%s", st))
-  stage_sims <- get(sprintf("stage_%s", st))
-  # probability of rejecting early
-  reject_early <- stage_sims == 1
-  last_stage_vals <- tstat_sims[!reject_early]
-  # p-value is prob. of rejecting early + more extreme in second stage
-  pval_tstat <- mean(!stage_sims) +
-    mean((last_stage_vals <= -abs(obs_tstat)) | (last_stage_vals >= abs(obs_tstat)))
-  pval_tstat_se <- sqrt(pval_tstat * (1 - pval_tstat) / N)
-
-  # save estimates
-  sim_pvals_smean <- c(sim_pvals_smean, pval_smean)
-  sim_pvals_tstat <- c(sim_pvals_tstat, pval_tstat)
-
-  # save standard errors
-  sim_pvals_smean_se <- c(sim_pvals_smean_se, pval_smean_se)
-  sim_pvals_tstat_se <- c(sim_pvals_tstat_se, pval_tstat_se)
-}
-
-setwd("~/repos/RCTCovarAdjust/simulations/")
-source("../R/constants.R")
-source("../R/pvalues.R")
-source("sim-utils.R")
-
-# OBF boundaries
-u_k <- c(2.963, 1.969)
-u_k1 <- cbind(-u_k, u_k)
-
-# This gives exactly alpha = 0.05 by putting in the last
-# boundary.
-n_K <- c(50, 100)
-
-anova <- get.pvalue.sm(obs=-1.969/sqrt(100), u_K=t(matrix(u_k1[1:1,])), n_K=n_K,
-                       ancova_monitor=F, ancova_test=F)
-get.pvalue.sm(obs=-1.969/sqrt(100), u_K=t(matrix(u_k1[1:1,])), n_K=n_K,
-              ancova_monitor=F, ancova_test=T, rho=0.5)
-
-vals.sm <- c()
-i.s <- seq(0.01, 0.99, 0.01)
-
-for(i in i.s){
-  cat(i, ": ")
-  res <- get.pvalue.sm(obs=-1.969/sqrt(100),
-                       u_K=t(matrix(u_k1[1:1,])), n_K=n_K,
-                       ancova_monitor=F, ancova_test=T, rho=i)
-  cat("\n")
-  vals.sm <- c(vals.sm, res)
-}
-
-# STAGE-WISE
-
-vals.sw <- c()
-anova.sw <- get.pvalue.sw(obs=-1.969,
-                          u_k=t(matrix(u_k1[1:1,])), n_k=n_K,
-                          ancova_monitor=F, ancova_test=F, rho=i,
-                          last_stage=T)
-for(i in i.s){
-  cat(i, ": ")
-  res <- get.pvalue.sw(obs=-1.969,
-                       u_k=t(matrix(u_k1[1:1,])), n_k=n_K,
-                       ancova_monitor=F, ancova_test=T, rho=i,
-                       last_stage=T)
-  cat("\n")
-  vals.sw <- c(vals.sw, res)
-}
-
-# PLOT
-par(mfrow=c(1, 2))
-# SAMPLE MEAN
-plot(vals.sm ~ i.s, xlab="rho", ylab="p-value", type='l', col='red',
-     ylim=c(0.04, 0.058),
-     main="Sample Mean")
-points(anova ~ 1, col='black', pch=16)
-abline(h=0.05, lty='dashed')
-
-# Simulations
-points(sim_pvals_smean ~ sim_rho, pch=16, col='blue')
-lower <- sim_pvals_smean - 1.96 * sim_pvals_smean_se
-upper <- sim_pvals_smean + 1.96 * sim_pvals_smean_se
-arrows(x0=sim_rho, y0=lower, x1=sim_rho, y1=upper, code=3, angle=90, length=0.05, col='blue')
-
-legend(x=0.6, y=0.062, col=c('red', 'black', 'blue'),
-       pch=c(NA, 16, 16), lty=c(1, NA, 1), legend=c("ANOVA",
-                                                    "ANCOVA",
-                                                    "Simulation"), cex=0.7)
-
-# STAGEWISE
-plot(vals.sw ~ i.s, xlab="rho", ylab="p-value", type='l', col='red',
-     ylim=c(0.04, 0.058),
-     main="Stage Wise")
-points(anova.sw ~ 1, pch=16, col='black')
-abline(h=0.05, lty='dashed')
-points(sim_pvals_tstat ~ sim_rho, pch=16, col='blue')
-
-# Simulations
-lower <- sim_pvals_tstat - 1.96 * sim_pvals_tstat_se
-upper <- sim_pvals_tstat + 1.96 * sim_pvals_tstat_se
-arrows(x0=sim_rho, y0=lower, x1=sim_rho, y1=upper, code=3, angle=90, length=0.05, col='blue')
-
-legend(x=0.6, y=0.062, col=c('red', 'black', 'blue'),
-       pch=c(NA, 16, 16), lty=c(1, NA, 1), legend=c("ANOVA",
-                                      "ANCOVA",
-                                      "Simulation"), cex=0.7)
+df[, n := as.factor(n)]
+write.csv(df, file=paste0(in_dir, "/summary.csv"), row.names=F)
 

487a509e33d799e8ad8a9bf8b1035ad74f86acf6
Marlena Bannick <mnorwood@uw.edu>
2022-05-30 21:49:41 -0700

    making tables script.

--- results/make-table1s.sh
+++ results/make-table1s.sh
@@ -7,7 +7,7 @@ R --no-save --args run-23-05-22-1 pocock < "table-1.R"
 R --no-save --args run-23-05-22-1 obf < "table-1.R"
 
 # Table A2
-# R --no-save --args . < "table-1.R"
+R --no-save --args run-30-05-22-7 pocock < "table-1.R"
 
 # Table A3
-# R --no-save --args . < "table-1.R"
+R --no-save --args run-30-05-22-8 pocock < "table-1.R"
